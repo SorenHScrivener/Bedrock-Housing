c522c996ad90990bf08090b940691af879f5914f
Soren H Scrivener <SorenHScrivener@gmail.com>
2022-04-04 18:46:27 -0400

    testing

--- wp-content/themes/cah/src/index.js
+++ wp-content/themes/cah/src/index.js
@@ -6,11 +6,11 @@ import '../css/dots.css'
 import Search from './modules/search';
 import Pagination from './modules/pagination';
 import News from './modules/all-news';
-import RelatedNews from './modules/singlePost';
+// import RelatedNews from './modules/singlePost';
 import ShadowBox from './modules/shadowBox';
 
 const search = new Search();
 const pagination = new Pagination();
 const news = new News();
-const relatedNews = new RelatedNews();
+// const relatedNews = new RelatedNews();
 const shadowBox = new ShadowBox();
