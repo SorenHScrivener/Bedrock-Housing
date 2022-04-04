e460c491fde14f80bcb72ccd4d03e768dbe20f4f
Soren H Scrivener <SorenHScrivener@gmail.com>
2022-04-04 18:39:52 -0400

    testing higher output threshold

--- wp-content/themes/cah/src/modules/pagination.js
+++ wp-content/themes/cah/src/modules/pagination.js
@@ -195,7 +195,7 @@ class Pagination {
             let dataCount = 0;
             let postOutput;
             console.log(window.innerWidth)
-            if(window.innerWidth >= 1060){
+            if(window.innerWidth >= 1360){
                 postOutput = 8;
             }else{
                 postOutput = 6;
